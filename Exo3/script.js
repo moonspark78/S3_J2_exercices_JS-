2871bde4ea28d99b4df4f76050d44789b2938031
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-05-20 14:43:09 +0200

    :bookmark:done  exo3  :bookmark:

---
+++ Exo3/script.js
@@ -0,0 +1,38 @@
+const entrepreneurs = [
+  { first: 'Steve', last: 'Jobs', year: 1955 },
+  { first: 'Oprah', last: 'Winfrey', year: 1954 },
+  { first: 'Bill', last: 'Gates', year: 1955 },
+  { first: 'Sheryl', last: 'Sandberg', year: 1969 },
+  { first: 'Mark', last: 'Zuckerberg', year: 1984 },
+  { first: 'Beyonce', last: 'Knowles', year: 1981 },
+  { first: 'Jeff', last: 'Bezos', year: 1964 },
+  { first: 'Diane', last: 'Hendricks', year: 1947 },
+  { first: 'Elon', last: 'Musk', year: 1971 },
+  { first: 'Marissa', last: 'Mayer', year: 1975 },
+  { first: 'Walt', last: 'Disney', year: 1901 },
+  { first: 'Larry', last: 'Page', year: 1973 },
+  { first: 'Jack', last: 'Dorsey', year: 1976 },
+  { first: 'Evan', last: 'Spiegel', year: 1990 },
+  { first: 'Brian', last: 'Chesky', year: 1981 },
+  { first: 'Travis', last: 'Kalanick', year: 1976 },
+  { first: 'Marc', last: 'Andreessen', year: 1971 },
+  { first: 'Peter', last: 'Thiel', year: 1967 }
+];
+
+let entrepreneursBornIn1970s = entrepreneurs.filter(entrepreneur => {
+  return entrepreneur.year >= 1970 && entrepreneur.year < 1980;
+});
+console.log(entrepreneursBornIn1970s);
+
+let entrepreneursFullName = entrepreneurs.map(entrepreneur => {
+  return entrepreneur.last;
+}
+);
+console.log(entrepreneursFullName);
+
+
+let currentYear = new Date().getFullYear();
+let entrepreneursCurrentAge = entrepreneurs.map(entrepreneur => {
+  return {name : entrepreneur.first, age: currentYear - entrepreneur.year};
+});
+console.log(entrepreneursCurrentAge);
